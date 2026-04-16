15fc99f8c2cb5220bbb8a85308eb431e8c9cd9e0
Augusto <augustovilasb@hotmail.com>
2026-04-16 19:33:01 +0100

    Student Grade Calculator

---
+++ src/ProgrammingLogic/AscendingOrder2.java
@@ -0,0 +1,13 @@
+package ProgrammingLogic;
+
+import java.util.Arrays;
+
+public class AscendingOrder2 {
+    public static void main(String[] args) {
+
+        int[] arr = {155, 5, 2566};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
