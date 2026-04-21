2269f8a115ce16ee1cdb1eccf0eca2b12cd7102b
Augusto <augustovilasb@hotmail.com>
2026-04-21 17:47:58 +0100

    Just trying a different way with ArrayList

--- src/ProgrammingLogic/AscendingOrder2.java
+++ src/ProgrammingLogic/AscendingOrder2.java
@@ -1,13 +1,26 @@
 package ProgrammingLogic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class AscendingOrder2 {
     public static void main(String[] args) {
 
-        int[] arr = {155, 5, 2566};
+/*        int[] arr = {155, 5, 2566};
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));*/
+
+        ArrayList<Double> numberList = new ArrayList<>();
+        numberList.add(3.68);
+        numberList.add(6.68);
+        numberList.add(7.68);
+        numberList.add(38.68);
+        Collections.sort(numberList);
+
+        for (Double numero : numberList) {
+        System.out.println(numero);
+        }
 
     }
 }
