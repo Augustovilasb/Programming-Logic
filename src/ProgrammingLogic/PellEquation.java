12ab706d6b7f8ec11a9cd610b03aeb515abb5539
Augusto <augustovilasb@hotmail.com>
2026-04-26 17:58:12 +0100

    Pell Equation using a Method to Simplify

--- src/ProgrammingLogic/PellEquation.java
+++ src/ProgrammingLogic/PellEquation.java
@@ -21,6 +21,30 @@ public class PellEquation {
             System.out.println(count + " run: " + number + " - " + odd + " = " + count);
             odd += 2;
         }
+
         System.out.println("Square root of " + inputNumber + " is: " + count);
+        System.out.println("The remainder is: " + number);
+        System.out.println("--------------");
+        System.out.println(" ");
+
+        System.out.println("Pell Equation with the remainder of Square of " + inputNumber);
+        System.out.println("Remainder is: " + number);
+        int newNumber = number * 100;
+        System.out.println(number + " * 100 " + newNumber);
+        System.out.println("--------------");
+        System.out.println(" ");
+
+        int number2 = newNumber;
+        int odd2 = 1;
+        int count2 = 0;
+
+        while (number2 > 0) {
+            if (number2 - odd2 < 0) break;
+            number2 = number2 - odd2;
+            count2++;
+            System.out.println(count2 + " run: " + number2 + " - " + odd2 + " = " + count2);
+            odd2 += 2;
+        }
+        System.out.println("√" + inputNumber + " ≈ " + count + "." + count2 / 10);
     }
 }
