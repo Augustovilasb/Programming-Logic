bec78bdd2000b2d0ececf1f4e9fd48b71ca2ef41
Augusto <augustovilasb@hotmail.com>
2026-04-26 16:49:20 +0100

    Login and password check

---
+++ src/ProgrammingLogic/LoginPage.java
@@ -0,0 +1,24 @@
+package ProgrammingLogic;
+
+import java.util.Scanner;
+
+public class LoginPage {
+    public static void main(String[] args) {
+
+        String login = "augustovilasb@hotmail.com";
+        String password = "senha0539wqk*";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the login: ");
+        String inputLogin = sc.nextLine();
+        System.out.println("Please enter the password: ");
+        String inputPassword = sc.nextLine();
+
+        if (login.equals(inputLogin) && password.equals(inputPassword)) {
+            System.out.println("Access available!!");
+        } else {
+            System.out.println("Access denied!!");
+        }
+
+    }
+}
