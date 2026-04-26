d0f51583fe7db2c143d075fceac8e3d4fae7dec4
Augusto <augustovilasb@hotmail.com>
2026-04-26 17:39:56 +0100

    Pell Equation finder Square root

---
+++ src/ProgrammingLogic/PellEquation.java
@@ -0,0 +1,26 @@
+package ProgrammingLogic;
+
+import java.util.Scanner;
+
+public class PellEquation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gimme the number: ");
+        int inputNumber = sc.nextInt();
+
+        int number = inputNumber;
+        int odd = 1;
+        int count = 0;
+
+        while (number > 0) {
+            if (number - odd < 0) break;
+            number = number - odd;
+            odd += 2;
+            count++;
+            System.out.println(count + " run: " + number + " - " + odd + " = " + count);
+        }
+        System.out.println("Square root of " + inputNumber + " is: " + count);
+    }
+}
